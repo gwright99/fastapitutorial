4a0e2e3f360524500c74243ad50aa78fa9f08f76
Graham Wright <graham.wright@seqera.io>
2024-09-15 08:50:07 -0400

    Testig logic to drop trailing slash.

--- app/main.py
+++ app/main.py
@@ -1,8 +1,17 @@
-from fastapi import APIRouter, FastAPI
+from fastapi import APIRouter, FastAPI, Request
 
 app = FastAPI(title="Recipe API", openapi_url="/openapi.json")
 
 api_router = APIRouter()
+
+
+# Problem with handling trailing slash in browswer
+# https://stackoverflow.com/questions/75726959/how-to-reroute-requests-to-a-different-url-endpoint-in-fastapi
+@app.middleware("http")
+async def some_middleware(request: Request, call_next):
+    if (request.url.path != "/") and (request.url.path)[-1] == "/":
+        request.scope["path"] = (request.scope["path"])[0:-1]
+    return await call_next(request)
 
 
 @api_router.get("/", status_code=200)
