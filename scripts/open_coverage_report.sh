d6dddd7010c7c751dd32e07245e79103147a395a
Graham Wright <graham.wright@seqera.io>
2024-10-14 18:30:51 -0400

    Testing act with PR

--- scripts/open_coverage_report.sh
+++ scripts/open_coverage_report.sh
@@ -6,8 +6,19 @@ original_dir=$PWD
 echo "Unzipping /tmp/act-artifacts/1"
 
 # 'unzip -o' == overwrite
-cd /tmp/act-artifacts/1/htmlcov
-unzip -o htmlcov
-firefox index.html
+# This won't work once I start suffixing reports based on python version. Replace with loop.
+# cd /tmp/act-artifacts/1/htmlcov
+# unzip -o htmlcov
+# firefox index.html
+
+versions="3.10.15 3.12.7"
+
+for version in ${versions}
+do
+    cd /tmp/act-artifacts/1/htmlcov-${version}
+    unzip -o htmlcov-${version}
+    firefox index.html
+    cd $original_dir
+done
 
 cd $original_dir
