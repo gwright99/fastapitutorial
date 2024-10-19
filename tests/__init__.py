402480a8c87e010c41d72b8d21ef4faf3d2a8c9a
Graham Wright <graham.wright@seqera.io>
2024-10-19 16:53:00 -0400

    [ci skip] Enabling POST transactions in HTTPRoute.

--- tests/__init__.py
+++ tests/__init__.py
@@ -2,7 +2,10 @@ import sys
 from pathlib import Path
 
 # Load `PROJECT_ROOT/` into `sys.path` for easy import
-base_import_dir = Path(__file__).resolve().parents[2]
+base_import_dir = Path(__file__).resolve().parents[1]
 
 if base_import_dir not in sys.path:
     sys.path.append(str(base_import_dir))
+    sys.path.append(f"{str(base_import_dir)}/app")
+
+print(sys.path)
