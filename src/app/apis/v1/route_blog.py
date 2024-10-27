f3a0df54bc4e1f50bca439ade620a0438e94f4f2
Graham Wright <graham.wright@seqera.io>
2024-10-27 09:32:53 -0400

    [ci skip] Added Blog retrieval endpoint.

--- src/app/apis/v1/route_blog.py
+++ src/app/apis/v1/route_blog.py
@@ -1,6 +1,6 @@
-from db.repository.blog import create_new_blog
+from db.repository.blog import create_new_blog, retrieve_blog
 from db.session import get_db
-from fastapi import APIRouter, Depends, status
+from fastapi import APIRouter, Depends, HTTPException, status
 from schemas.blog import CreateBlog, ShowBlog
 from sqlalchemy.orm import Session
 
@@ -13,4 +13,15 @@ async def create_blog(blog: CreateBlog, db: Session = Depends(get_db)):
     blog = create_new_blog(
         blog=blog, db=db, author_id=1
     )  # TODO: Make author_id variable.
+    return blog
+
+
+@router.get("/blog/{id}", response_model=ShowBlog)
+def get_blog(id: int, db: Session = Depends(get_db)):
+    blog = retrieve_blog(id=id, db=db)
+    if not blog:
+        raise HTTPException(
+            detail=f"Blog with ID {id} does not exist.",
+            status_code=status.HTTP_404_NOT_FOUND,
+        )
     return blog
