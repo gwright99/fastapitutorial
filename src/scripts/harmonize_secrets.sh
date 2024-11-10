e28219e506d135b252990bcf293289de035b38c2
Graham Wright <graham.wright@seqera.io>
2024-11-10 14:58:43 -0500

    [ci skip] WIP deploying .env via secret.

--- src/scripts/harmonize_secrets.sh
+++ src/scripts/harmonize_secrets.sh
@@ -7,5 +7,7 @@ scp .env testcontrol1:/tmp/fastapi.env
 
 # K8s secret must be lowercase
 ssh testcontrol1 << EOF
-kubectl create secret generic envfile -n fastapi --from-env-file=/tmp/fastapi.env
+kubectl delete secret envfile -n fastapi  || true
+# kubectl create secret generic envfile -n fastapi --from-env-file=/tmp/fastapi.env
+kubectl create secret generic envfile --from-literal=.env=$(cat /tmp/fastapi.env)
 EOF
